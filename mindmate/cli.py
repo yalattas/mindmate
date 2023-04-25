54b48b7d7ea4dc8c6dbadbb0efe194ce8b05b298
Yaser Alattas <y.alattas@gmail.com>
2023-04-25 08:26:43 +0300

    feat(command): ai to list AI-platforms

--- mindmate/cli.py
+++ mindmate/cli.py
@@ -1,19 +1,23 @@
 import click
-from .utils import helper
-from .commands.configure import configure
-from .commands.initdb import initdb
-from .commands.dropdb import dropdb
-from .commands.nested_group import nested_group
+from mindmate.utils import helper
+from mindmate.commands.configure import configure
+from mindmate.commands.chat import chat
+from mindmate.commands.ai import ai
+from mindmate.commands.initdb import initdb
+from mindmate.commands.dropdb import dropdb
 
-@click.group()
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.group(context_settings=CONTEXT_SETTINGS)
 def cli():
     """hi to mindmate cli, try option --help for more information"""
     pass
 
 cli.add_command(configure)
+cli.add_command(chat)
 cli.add_command(initdb)
 cli.add_command(dropdb)
-cli.add_command(nested_group)
+cli.add_command(ai)
 
 if __name__ == '__main__':
     cli()
--- mindmate/commands/configure.py
+++ mindmate/commands/configure.py
@@ -1,39 +1,36 @@
 import click
 import yaml
+import uuid
 import os
-
-def set_yaml_state(file: str) -> dict:
-    """read yaml file and return it as a dictionary object"""
-    with open(file, 'r') as f:
-        data_dict = yaml.load(f, Loader=yaml.FullLoader)
-    return data_dict
-def update_yaml_state(state: dict, file_path: str) -> None:
-    """update new yaml state with only changed keys and maintain unchanged values"""
-    with open(file_path, 'w') as outputfile:
-        yaml.dump(state, outputfile, default_flow_style=False)
+from mindmate.utils.utils import utility
+from mindmate.utils.conf import constants
 
 @click.command()
 def configure():
     """collect keys from user and save it into state file as yaml"""
 
-    PATH = '~/.mindmate'
-    FILE_NAME = 'environment.yaml'
+    # PATH = '~/.mindmate'
+    # FILE_NAME = 'environment.yaml'
 
-    while not os.path.isfile(PATH+'/'+FILE_NAME):
+    while not os.path.isfile(constants.FILE_PATH+'/'+constants.FILE_NAME):
         try:
-            os.makedirs(PATH)
+            os.makedirs(constants.FILE_PATH)
         except FileExistsError as f:
             pass
         data = {
+            'version':1,
             'keys': {
-                'openai_token':'xxxx'
+                'openai_token':'xxxx',
+                'openai_id':'xxxx',
             }
         }
-        with open(PATH+'/'+FILE_NAME, 'w') as outputfile:
+        with open(constants.FILE_PATH+'/'+constants.FILE_NAME, 'w') as outputfile:
             yaml.dump(data, outputfile, default_flow_style=False)
 
-    file = set_yaml_state(PATH+'/'+FILE_NAME)
+    file = utility.set_yaml_state(constants.FILE_PATH+'/'+constants.FILE_NAME)
+    #TODO: check environment variable first before setting values into yaml file. If exist, then skip yaml update
     openai_value = file['keys']['openai_token']
     openai_user_token = click.prompt(f'OPENAI TOKEN [****************{openai_value[-4:]}]', type=str)
     file['keys']['openai_token'] = openai_user_token
-    update_yaml_state(state=file, file_path=PATH+'/'+FILE_NAME)
+    file['keys']['openai_id'] = str(uuid.uuid4())
+    utility.update_yaml_state(state=file, file_path=constants.FILE_PATH+'/'+constants.FILE_NAME)
