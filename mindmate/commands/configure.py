943260f321c52dab7096394f12a3a1cfc0a538a1
Yaser Alattas <y.alattas@gmail.com>
2023-04-26 05:14:50 +0300

    hotfix(chat command): validate existence of credentials first

--- mindmate/commands/configure.py
+++ mindmate/commands/configure.py
@@ -1,31 +1,11 @@
 import click
-import yaml
 import uuid
-import os
 from mindmate.utils.utils import utility
 from mindmate.utils.conf import constants
 
 @click.command()
 def configure():
     """collect keys from user and save it into state file as yaml"""
-
-    # PATH = '~/.mindmate'
-    # FILE_NAME = 'environment.yaml'
-
-    while not os.path.isfile(constants.FILE_PATH+'/'+constants.FILE_NAME):
-        try:
-            os.makedirs(constants.FILE_PATH)
-        except FileExistsError as f:
-            pass
-        data = {
-            'version':1,
-            'keys': {
-                'openai_token':'xxxx',
-                'openai_id':'xxxx',
-            }
-        }
-        with open(constants.FILE_PATH+'/'+constants.FILE_NAME, 'w') as outputfile:
-            yaml.dump(data, outputfile, default_flow_style=False)
 
     file = utility.set_yaml_state(constants.FILE_PATH+'/'+constants.FILE_NAME)
     #TODO: check environment variable first before setting values into yaml file. If exist, then skip yaml update
